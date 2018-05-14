5c386b224dcf13dc82672912ec655f3438bc4512
saikumar92 <saikumar92@users.noreply.github.com>
2018-05-14 12:19:54 -0400

    bootstrap update

---
+++ SportsAggregator/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+
+namespace SportsAggregator
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap_flatly.css"));
+                      //"~/Content/site.css"));
+        }
+    }
+}
